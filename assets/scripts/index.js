01b6f5234af7e25a681b5db70ed04492fc59fbf0
laurenallegra <lauren.allegra@gmail.com>
2016-09-07 14:21:32 -0400

    Add basic working html, scss, js
    Should have done initial commit after just html; got caught up coding.
    Added code from html-css-layout-bootstrap-sass lesson (Brian) and
    made alterations to html.
    
    Also commented out part of event-handlers that makes 'sign-in' modal pop-up
    upon pageload (to easier manipulate and view html & css).

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,7 +1,27 @@
 'use strict';
 
-// user require with a reference to bundle the file and use it in this file
+// use require with a reference to bundle the file and use it in this file
 // var example = require('./example');
 
 // use require without a reference to ensure a file is bundled
 require('./example');
+
+// event handlers and stuff
+
+$('#sign-in').on('click', function() {
+$('#signInModal').modal('show');
+});
+
+$('#sign-in-button').on('click', function() {
+$('#signInModal').modal('hide');
+$('#sign-out').show();
+$('#change-password').show();
+$('#sign-up').hide();
+$('#sign-in').hide();
+});
+
+// $(document).ready(function() {
+// $('#sign-out').hide();
+// $('#change-password').hide();
+// $('#signInModal').modal('show');
+// });
