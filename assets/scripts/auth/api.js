9fb6c101a7824f422f89726240dd35d7a621d6f6
laurenallegra <lauren.allegra@gmail.com>
2016-09-08 10:22:54 -0400

    Add auth folder w/ api.js, events.js, ui.js files
    Created `auth` folder ('assets/scripts/auth') and 3 JS files:
      -api.js
      -events.js
      -ui.js
    
    Copied and pasted contents of corresponding files from
    `api-token-auth` training, as starter code to use as a template.
    
    Also added forms for sign-up, sign-in, change-password, and sign-out
    in html file--(not sure if I will use these, modals, or combination
    of both)

---
+++ assets/scripts/auth/api.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const app = require('../app');
+
+const signUp = (data) => {
+  console.log(data);
+return $.ajax({
+    url:app.host + '/sign-up',
+    method: 'POST',
+    data: data,
+  });
+};
+
+const signIn = (data) => {
+    return $.ajax({
+      url:app.host + '/sign-in',
+      method: 'POST',
+      data: data,
+    });
+};
+
+const changePassword = (data) => {
+    return $.ajax({
+      url: app.host + '/change-password/' + app.user.id,
+      method: 'PATCH',
+      headers: {
+      Authorization: 'Token token=' + app.user.token,
+      },
+      data: data,
+    });
+};
+
+const signOut = () => {
+    return $.ajax({
+      url: app.host + '/sign-out/' + app.user.id,
+      method: 'DELETE',
+      headers: {
+      Authorization: 'Token token=' + app.user.token,
+      },
+    });
+};
+
+//DELETED CODE BELOW:
+  // if (Math.random() > 0.5) {
+  //   success('in signUp');
+  // } else {
+  //   let error = new Error('Random');
+  //   error.data = data;
+  //   failure(error);
+  //}
+//};
+
+module.exports = {
+  signUp,
+  signIn,
+  signOut,
+  changePassword,
+};
---
+++ assets/scripts/auth/ui.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const app = require('../app');
+
+const signInSuccess = (data) => {
+  app.user = data.user;
+};
+
+const changePasswordSuccess = () => {
+  console.log('password successfully changed');
+};
+
+const success = (data) => {
+  console.log(data);
+};
+
+const failure = (error) => {
+  console.error(error);
+};
+
+const signOutSuccess = () => {
+  app.user = null;
+  console.log("Successfully signed out.");
+};
+
+module.exports = {
+  failure,
+  success,
+  signInSuccess,
+  changePasswordSuccess,
+  signOutSuccess,
+};
